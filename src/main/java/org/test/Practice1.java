8882e370e8d90708edbd0ff4408f8b16ea1ee1e8
praveenkv0 <praveenboss712@gmail.com>
2020-08-06 18:21:24 +0530

    done

---
+++ src/main/java/org/test/Practice1.java
@@ -0,0 +1,11 @@
+package org.test;
+
+public class Practice1 {
+	 static int a = 100;
+	static int b= 20;
+	public static void main(String[] args) {
+		int c= a+b;
+		System.out.println(c);
+		
+	}
+}
